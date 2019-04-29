d78453f6cb816b2e8ac2ed701351af0f1c3572c9
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-04-29 15:18:01 +0800

    Update model_ngappw.py

--- model_ngappw.py
+++ model_ngappw.py
@@ -55,21 +55,21 @@ if __name__ == '__main__':
 
                                     while now_budget < begin_budget and mep_i_node != '-1':
                                         sc = seed_cost_dict[mep_i_node]
-                                        if now_budget + sc >= begin_budget and begin_budget < total_budget and len(temp_sequence) == 0:
+                                        seed_set_length = sum(len(seed_set[kk]) for kk in range(num_product))
+                                        if round(now_budget + sc, 2) >= begin_budget and begin_budget < total_budget and mep_flag == seed_set_length and len(temp_sequence) == 0:
                                             ss_time = round(time.time() - ss_strat_time + ss_acc_time, 2)
                                             temp_celf_sequence = copy.deepcopy(celf_sequence)
                                             temp_celf_sequence.insert(0, mep_g)
                                             temp_sequence.append([begin_budget + 1, now_budget, now_profit, copy.deepcopy(seed_set), copy.deepcopy(expected_profit_k),
                                                                   temp_celf_sequence, ss_time])
 
-                                        if now_budget + sc > begin_budget:
+                                        if round(now_budget + sc, 2) >= begin_budget > begin_budget:
                                             mep_g = celf_sequence.pop(0)
                                             mep_k_prod, mep_i_node, mep_mg, mep_flag = mep_g
                                             if mep_i_node == '-1':
                                                 break
                                             continue
 
-                                        seed_set_length = sum(len(seed_set[kk]) for kk in range(num_product))
                                         if mep_flag == seed_set_length:
                                             now_profit = round(now_profit + mep_mg, 4)
                                             now_budget = round(now_budget + seed_cost_dict[mep_i_node], 2)
